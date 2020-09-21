a679d29409d2fdabc9885e43bab338c9f3060a10
赵雷涛 <819539651@qq.com>
2020-09-21 18:11:14 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App',
+  async mounted() {
+    console.log('mounted start')
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log('i m mounted')
+        resolve()
+      }, 1000)
+    })
+
+    console.log('mounted end')
+  }
+})
+</script>
